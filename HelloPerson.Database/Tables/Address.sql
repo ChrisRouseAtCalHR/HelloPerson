9f33e0e5a8301e396ce56e91c69d4cb789d37614
Christopher Rouse <chris.rouse@calhr.ca.gov>
2017-11-09 11:48:10 -0800

    Initial Chaos

---
+++ HelloPerson.Database/Tables/Address.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Address]
+(
+	[AddressId] INT NOT NULL,
+	[PersonId] INT NOT NULL,
+	[Line1] VARCHAR(50) NOT NULL,
+	[Line2] VARCHAR(50) NOT NULL,
+	[City] VARCHAR(50) NOT NULL,
+	[State] CHAR(2) NOT NULL,
+	[ZipCode] CHAR(10) NOT NULL,
+	CONSTRAINT [pk_Address] PRIMARY KEY ([AddressId]),
+	CONSTRAINT [fk_Address_PersonId] FOREIGN KEY ([PersonId]) REFERENCES [dbo].[Person]([PersonId])
+);
+
+GO
+
+CREATE INDEX [ix_Address_PersonId] ON [dbo].[Address]([PersonId]);
