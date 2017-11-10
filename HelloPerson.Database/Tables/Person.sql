686483bdc8ea0796289bfab0b4bb645de9253b6b
Chris Rouse <nerdlyhaxor@gmail.com>
2017-11-10 14:01:48 -0800

    Added phone and reformatted tables.

--- HelloPerson.Database/Tables/Person.sql
+++ HelloPerson.Database/Tables/Person.sql
@@ -1,9 +1,8 @@
 ﻿CREATE TABLE [dbo].[Person]
 (
-	[PersonId] INT NOT NULL,
-	[FirstName] VARCHAR(50) NOT NULL,
-	[LastName] VARCHAR(50) NOT NULL,
+	[PersonId]   INT NOT NULL,
+	[FirstName]  VARCHAR(50) NOT NULL,
+	[LastName]   VARCHAR(50) NOT NULL,
 	[CreateDate] DATETIME NOT NULL,
-	CONSTRAINT [pk_Person] PRIMARY KEY ([PersonId])
-
+	CONSTRAINT [pk_Person] PRIMARY KEY([PersonId])
 );
---
+++ HelloPerson.Database/Tables/Phone.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Phone]
+(
+	[PhoneId]   INT NOT NULL,
+	[PersonId]  INT NOT NULL,
+	[Number]    VARCHAR(10) NOT NULL,
+	[Extension] CHAR(4) NULL,
+	CONSTRAINT [pk_Phone] PRIMARY KEY([PhoneId]),
+	CONSTRAINT [fk_Phone_PersonId] FOREIGN KEY([PersonId]) REFERENCES [dbo].[Person]([PersonId])
+);
